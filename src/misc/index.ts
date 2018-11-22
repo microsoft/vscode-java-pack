75a0fd295b9e6a0c586714a570e7231f0ed79bc3
akaroml <rome.li@microsoft.com>
2018-11-22 18:43:53 +0800

    Add missing file

---
+++ src/misc/index.ts
@@ -0,0 +1,13 @@
+import * as vscode from "vscode";
+
+function showInfoButton() {
+  let infoButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+  infoButton.command = "java.overview";
+  infoButton.text = "$(info) Java";
+  infoButton.tooltip = "Learn more about Java features";
+  infoButton.show();
+}
+
+export function initialize(context: vscode.ExtensionContext) {
+  showInfoButton();
+}
