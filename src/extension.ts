84a8f424b0e0e1cc55a14a5b45156bdba0ff93f3
akaroml <rome.li@microsoft.com>
2018-12-05 09:59:36 +0800

    Add license header

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 import * as vscode from 'vscode';
 import { dispose as disposeTelemetryWrapper, initialize, instrumentOperation } from 'vscode-extension-telemetry-wrapper';
 
