c547b32280f72806ff063891f6345ae9ec3a42c9
Rome Li <rome.li@microsoft.com>
2019-08-31 10:16:03 +0800

    Getting Started - Code Editing (#184)
    * Getting Started - Code Editing
    
    Signed-off-by: Rome Li <rome.li@microsoft.com>
    
    * Fix ts/build error
    
    Signed-off-by: Rome Li <rome.li@microsoft.com>

--- src/getting-started/assets/index.ts
+++ src/getting-started/assets/index.ts
@@ -6,5 +6,13 @@ import "bootstrap/js/src/tab";
 import * as $ from "jquery";
 
 $("#navigationPanel a").click(e => {
-  $($(e.target).attr("href")).tab('show');
+  ($($(e.target).attr("href")||"") as any).tab("show");
 });
+
+let os = "win";
+if (navigator.platform.toLowerCase().indexOf("mac") === 0) {
+  os = "mac";
+}
+
+const osToHide = os === "win" ? "mac" : "win";
+$(`kbd[data-os=${osToHide}]`).hide();
