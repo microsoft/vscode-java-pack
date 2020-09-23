f953764ba6a334a7fc579b7a4b236c1e8e6cd6fb
xqzlgy2 <xqzlgy2@gmail.com>
2020-09-23 10:18:20 +0800

    Reformat file

--- src/survey/constants.ts
+++ src/survey/constants.ts
@@ -2,34 +2,34 @@
 // Licensed under the MIT license.
 
 export const BannerActions = {
-    bannerLabelYes: 'Take Survey',
-    bannerLabelNo: 'Don\'t show again',
-    remindLater: 'Remind Me later'
+  bannerLabelYes: 'Take Survey',
+  bannerLabelNo: 'Don\'t show again',
+  remindLater: 'Remind Me later'
 }
 
 export const SurveyBanner = {
-    message: 'Do you mind taking a quick feedback survey about the Java extension pack?',
-    action: [
-        BannerActions.bannerLabelYes,
-        BannerActions.bannerLabelNo,
-        BannerActions.remindLater
-    ],
-    FIRST_STAGE_URL: 'https://www.microsoft.com',
-    SECOND_STAGE_URL: 'https://www.microsoft.com',
-    THIRD_STAGE_URL: 'https://www.microsoft.com',
+  message: 'Do you mind taking a quick feedback survey about the Java extension pack?',
+  action: [
+    BannerActions.bannerLabelYes,
+    BannerActions.bannerLabelNo,
+    BannerActions.remindLater
+  ],
+  FIRST_STAGE_URL: 'https://www.microsoft.com',
+  SECOND_STAGE_URL: 'https://www.microsoft.com',
+  THIRD_STAGE_URL: 'https://www.microsoft.com',
 }
 
 export enum SurveyKeys {
-    FIRST_ACTIVATION_TIME = 'packFirstActicationTime',
-    DISABLED_SURVEY_UNTIL = 'packDisableSurveyUntil',
-    DO_NOT_SHOW_AGAIN = 'packDoNotShowSurveyAgain'
+  FIRST_ACTIVATION_TIME = 'packFirstActicationTime',
+  DISABLED_SURVEY_UNTIL = 'packDisableSurveyUntil',
+  DO_NOT_SHOW_AGAIN = 'packDoNotShowSurveyAgain'
 }
 
 export enum SurveyTimespan {
-    FIRST_DAY = 1000 * 60 * 60 * 24, // 1 day
-    SURVEY_DISABLE_TIME = 1000 * 60 * 60 * 24 * 3, // 3 days
-    FIRST_STAGE = 1000 * 60 * 60 * 24 * 7, // 1 week
-    SECOND_STAGE = 1000 * 60 * 60 * 24 * 21 // 3 weeks
+  FIRST_DAY = 1000 * 60 * 60 * 24, // 1 day
+  SURVEY_DISABLE_TIME = 1000 * 60 * 60 * 24 * 3, // 3 days
+  FIRST_STAGE = 1000 * 60 * 60 * 24 * 7, // 1 week
+  SECOND_STAGE = 1000 * 60 * 60 * 24 * 21 // 3 weeks
 }
 
 export const PROBABILITY = 0.1;
