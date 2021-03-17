b50c8d62b402f371567cdb0718af9ecf2cf4c0f0
Sheng Chen <sheche@microsoft.com>
2021-03-16 21:52:01 -0700

    Extract the common types (#568)
    Signed-off-by: Sheng Chen <sheche@microsoft.com>

--- src/classpath/assets/utils.ts
+++ src/classpath/assets/utils.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import { ProjectType } from "../types";
+import { ProjectType } from "../../utils/webview";
 
 export const WEBVIEW_ID = "java.classpathConfiguration";
 
--- src/classpath/types.ts
+++ src/classpath/types.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+import { ProjectType } from "../utils/webview";
+
 export interface ProjectInfo {
     name: string;
     rootPath: string;
@@ -11,20 +13,6 @@ export interface ClasspathComponent {
     sourcePaths: string[];
     defaultOutputPath: string;
     referenceLibraries: string[];
-}
-
-export enum ProjectType {
-    Default = "Default project",
-    UnmanagedFolder = "Unmanaged folder",
-    Maven = "Maven",
-    Gradle = "Gradle",
-    Others = "Others",
-}
-
-export enum NatureId {
-    Maven = "org.eclipse.m2e.core.maven2Nature",
-    Gradle = "org.eclipse.buildship.core.gradleprojectnature",
-    Java = "org.eclipse.jdt.core.javanature",
 }
 
 export enum ClasspathViewException {
