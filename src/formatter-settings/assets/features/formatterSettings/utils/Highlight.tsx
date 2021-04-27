73a4b25ee516a11776185728df45e943ff0f040e
Shi Chen <chenshi@microsoft.com>
2021-04-27 13:02:10 +0800

    try innerts

--- src/formatter-settings/assets/features/formatterSettings/utils/Highlight.tsx
+++ src/formatter-settings/assets/features/formatterSettings/utils/Highlight.tsx
@@ -9,27 +9,9 @@ import { DOMParser, XMLSerializer } from "xmldom";
 export function Highlighter(content: string, language?: string): JSX.Element {
   const highlighted = language ? hljs.highlight(language, content) : hljs.highlightAuto(content);
   const document = new DOMParser().parseFromString(highlighted.value);
-  if (document) {
-    const childNodes = document.childNodes;
-    if (childNodes) {
-      for (let i = 0; i < childNodes.length; i++) {
-        let child = childNodes.item(i);
-        if (child.nodeType === 3) {
-          child = child as CharacterData;
-          child.appendChild(child);
-          //child.nodeValue = child.nodeValue.replace(" ", "\u00B7");
-          //child.data = child.nodeValue;
-          const test2 = 0;
-        }
-        const test1 = 0;
-      }
-    } else {
-      const test = 1;
-    }
-  }
   return (
     <pre className="hljs d-flex flex-grow-1">
-      <code className="hljs flex-grow-1" dangerouslySetInnerHTML={{ __html: new XMLSerializer().serializeToString(document) }} />
+      <code className="hljs flex-grow-1" dangerouslySetInnerHTML={{ __html: highlighted.value }} />
     </pre>
   );
 }
