c42f15f8b3cbb26b6520891a2fcef72f52868986
Yan Zhang <Eskibear@users.noreply.github.com>
2021-05-21 14:22:00 +0800

    add shortcut: Jyputer Notebook for Java (#619)
    Signed-off-by: Yan Zhang <yanzh@microsoft.com>

--- src/welcome/assets/components/NavigationPanel.tsx
+++ src/welcome/assets/components/NavigationPanel.tsx
@@ -40,7 +40,8 @@ export default class NavigationPanel extends React.Component {
         { name: "Getting Started", command: "java.gettingStarted" },
         { name: "Tutorial: Running and Debugging", command: "java.helper.openUrl", args: ["https://code.visualstudio.com/docs/java/java-debugging"] },
         { name: "Tutorial: Testing", command: "java.helper.openUrl", args: ["https://code.visualstudio.com/docs/java/java-testing"] },
-        { name: "Configure Sources, Dependencies, Output Folder...", command: "java.classpathConfiguration"}
+        { name: "Configure Sources, Dependencies, Output Folder...", command: "java.classpathConfiguration" },
+        { name: "Quick Start: Jupyter Notebook for Java", command: "java.helper.openUrl", args: ["https://github.com/microsoft/vscode-java-pack/wiki/Quick-Start:-Jupyter-Notebook-for-Java"] }
       ]
     },
   ];
