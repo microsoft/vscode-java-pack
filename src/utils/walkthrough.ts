22ce1fb191262eff188e6ec5da03b94c0b5b09c6
Yan Zhang <yanzh@microsoft.com>
2021-08-02 16:35:34 +0800

    perf: cache exp result for not-started experiment
    Signed-off-by: Yan Zhang <yanzh@microsoft.com>

--- src/utils/walkthrough.ts
+++ src/utils/walkthrough.ts
@@ -5,11 +5,15 @@ import { workspace } from "vscode";
 import { getExpService } from "../exp";
 import { TreatmentVariables } from "../exp/TreatmentVariables";
 
-export function isWalkthroughEnabled() {
-
-    const fromExp = getExpService()?.getTreatmentVariableAsync<boolean>(TreatmentVariables.VSCodeConfig, TreatmentVariables.JavaWalkthroughEnabled, true /* checkCache */) ?? false;
-    // 	can be overridden by local settings "experiments.override.gettingStarted.overrideCategory.vscjava.vscode-java-pack#javaWelcome.when": "true"
-    const fromSettings = workspace.getConfiguration("experiments.override.gettingStarted.overrideCategory").get<string>("vscjava.vscode-java-pack#javaWelcome.when") === "true";
+// local cache before EXP starts, otherwise it always returns undefined, and keeps querying service.
+let fromExpCache: boolean;
+export async function isWalkthroughEnabled() {
+    const fromExp = fromExpCache ?? await getExpService()?.getTreatmentVariableAsync<boolean>(TreatmentVariables.VSCodeConfig, TreatmentVariables.JavaWalkthroughEnabled, true /* checkCache */) ?? false;
+    fromExpCache = fromExp;
+    
+    // can be overridden by local settings "experiments.override.gettingStarted.overrideCategory.vscjava.vscode-java-pack.javaWelcome.when": "true"
+    // '#' -> '.' since https://github.com/microsoft/vscode/commit/fe671f300845ca5161885125b1e12d43fc25ccf8
+    const fromSettings = workspace.getConfiguration("experiments.override.gettingStarted.overrideCategory").get<string>("vscjava.vscode-java-pack.javaWelcome.when") === "true";
 
     return fromExp || fromSettings;
 }
