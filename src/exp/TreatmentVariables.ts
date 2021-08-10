85002d514688904433e64460a99a4683d76fbbd5
Yan Zhang <yanzh@microsoft.com>
2021-08-10 13:55:02 +0800

    fix exp to query insiders population
    Signed-off-by: Yan Zhang <yanzh@microsoft.com>

--- src/exp/TreatmentVariables.ts
+++ src/exp/TreatmentVariables.ts
@@ -4,5 +4,5 @@
 export class TreatmentVariables {
     public static readonly VSCodeConfig = 'vscode';
     public static readonly PresentWelcomePageByDefault = 'presentWelcomePageByDefault';
-    public static readonly JavaWalkthroughEnabled = "vscode.gettingStarted.overrideCategory.vscjava.vscode-java-pack.javaWelcome.when"
+    public static readonly JavaWalkthroughEnabled = "gettingStarted.overrideCategory.vscjava.vscode-java-pack.javaWelcome.when"
 }
--- src/utils/walkthrough.ts
+++ src/utils/walkthrough.ts
@@ -6,14 +6,23 @@ import { getExpService } from "../exp";
 import { TreatmentVariables } from "../exp/TreatmentVariables";
 
 // local cache before EXP starts, otherwise it always returns undefined, and keeps querying service.
-let fromExpCache: boolean;
+let enabledByExp: boolean;
+
 export async function isWalkthroughEnabled() {
-    const fromExp = fromExpCache ?? await getExpService()?.getTreatmentVariableAsync<boolean>(TreatmentVariables.VSCodeConfig, TreatmentVariables.JavaWalkthroughEnabled, true /* checkCache */) ?? false;
-    fromExpCache = fromExp;
-    
     // can be overridden by local settings "experiments.override.gettingStarted.overrideCategory.vscjava.vscode-java-pack.javaWelcome.when": "true"
     // '#' -> '.' since https://github.com/microsoft/vscode/commit/fe671f300845ca5161885125b1e12d43fc25ccf8
-    const fromSettings = workspace.getConfiguration("experiments.override.gettingStarted.overrideCategory").get<string>("vscjava.vscode-java-pack.javaWelcome.when") === "true";
+    const fromSettings: string | undefined = workspace.getConfiguration("experiments.override.gettingStarted.overrideCategory").get<string>("vscjava.vscode-java-pack.javaWelcome.when");
+    switch (fromSettings) {
+        case "true":
+            return true;
+        case "false":
+            return false;
+        default:
+    }
 
-    return fromExp || fromSettings;
+    if (enabledByExp === undefined) {
+        const flightValue = await getExpService()?.getTreatmentVariableAsync<string>(TreatmentVariables.VSCodeConfig, TreatmentVariables.JavaWalkthroughEnabled, true /* checkCache */);
+        enabledByExp = (flightValue === "true");
+    }
+    return enabledByExp;
 }
