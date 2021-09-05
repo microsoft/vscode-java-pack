acd75336c703f333326dbac68d5c033b83b717a8
Yan Zhang <Eskibear@users.noreply.github.com>
2021-09-06 00:22:36 +0800

    fix broken link to blogs (#735)
    Signed-off-by: Yan Zhang <yanzh@microsoft.com>

--- src/welcome/assets/components/Header.tsx
+++ src/welcome/assets/components/Header.tsx
@@ -6,9 +6,10 @@ import { encodeCommandUriWithTelemetry } from "../../../utils/webview";
 import { WEBVIEW_ID } from "../utils";
 const logo = require("../../../../logo.svg");
 
+const DEV_BLOG_LINK = "https://devblogs.microsoft.com/?s=Java+on+Visual+Studio+Code";
 export default class Header extends React.Component {
   render() {
-    const openBlogCommand = encodeCommandUriWithTelemetry(WEBVIEW_ID, "blogs", "java.helper.openUrl", ["https://devblogs.microsoft.com/java/?s=Java+on+Visual+Studio+Code"]);
+    const openBlogCommand = encodeCommandUriWithTelemetry(WEBVIEW_ID, "blogs", "java.helper.openUrl", [DEV_BLOG_LINK]);
     const title = <h2>Java Tools</h2>;
     const blogsLink = <a href={openBlogCommand}>blogs</a>;
     const subtitle = <span>Check our {blogsLink} to see what's new</span>;
