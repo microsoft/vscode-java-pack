d8e68ee2a6101c17ff7c7bd409ab2b42e1a8ab3b
Yan Zhang <yanzh@microsoft.com>
2022-01-13 14:53:53 +0800

    jcmd to query uptime, parse log for errors
    Signed-off-by: Yan Zhang <yanzh@microsoft.com>

--- src/daemon/index.ts
+++ src/daemon/index.ts
@@ -1,9 +1,114 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import { ExtensionContext } from "vscode";
+import * as cp from "child_process";
+import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
+import { promisify } from "util";
+import * as vscode from "vscode";
 import { LogWatcher } from "./logWatcher";
 
-export function initDaemon(context: ExtensionContext) {
-   new LogWatcher(context).start(); 
+
+const execFile = promisify(cp.execFile);
+const delay = promisify(setTimeout);
+
+interface IJdtlsMetadata {
+   pid?: string;
+   jreHome?: string;
+   workspace?: string;
+}
+
+export async function initDaemon(context: vscode.ExtensionContext) {
+   const jdtlsMetadata = await getJdtlsMetadata(context);
+   console.log(jdtlsMetadata);
+   const uptime = await getUpTime(jdtlsMetadata?.jreHome!, jdtlsMetadata?.pid!);
+   console.log("javaExt activated:", uptime);
+
+   new LogWatcher(context).start();
+}
+
+async function getJdtlsMetadata(context: vscode.ExtensionContext): Promise<IJdtlsMetadata | undefined> {
+   const javaExt = vscode.extensions.getExtension("redhat.java");
+   if (!javaExt) {
+      return undefined;
+   }
+
+   // get embedded JRE Home
+   let jreHome: string | undefined;
+   try {
+      const jreFolder = path.join(javaExt.extensionPath, "jre");
+      const jreDistros = await fs.promises.readdir(jreFolder);
+      if (jreDistros.length > 0) {
+         jreHome = path.join(jreFolder, jreDistros[0]);
+      }
+   } catch (error) {
+      console.error(error);
+   }
+   if (!jreHome) {
+      return undefined;
+   }
+
+   // wait javaExt to activate
+   const timeout = 10000;
+   let count = 0;
+   while(!javaExt.isActive && count < timeout) {
+      await delay(1000);
+      console.log("waiting");
+      count += 1000;
+   }
+
+   // on ServiceReady
+   javaExt.exports.onDidServerModeChange(async (mode: string) => {
+      console.log(mode);
+      if (mode === "Standard") {
+         const serviceReadyTime = await getUpTime(jreHome!, pid!);
+         console.log("serviceReady:", serviceReadyTime)
+      }
+   });
+
+   // get PID and jdtls.workspace
+   const {pid, workspace} = await getPidAndWS(jreHome, context);
+
+
+   return {
+      jreHome,
+      pid,
+      workspace
+   }
+
+}
+
+function parseJdtlsJps(jdtlsJpsLine: string): IJdtlsMetadata {
+   const spaceIdx = jdtlsJpsLine.indexOf(" ");
+   const pid = jdtlsJpsLine.slice(0, spaceIdx);
+   const cmd = jdtlsJpsLine.slice(spaceIdx + 1);
+   const res = cmd.match(/-XX:HeapDumpPath=(.*(redhat.java|vscodesws_[0-9a-f]{5}))/);
+   let workspace;
+   if (res && res[1]) {
+      workspace = res[1];
+   }
+
+   return {
+      pid,
+      workspace
+   };
+}
+
+async function getUpTime(jreHome: string, pid: string): Promise<string> {
+   const execRes = await execFile(path.join(jreHome, "bin", "jcmd"), [pid, "VM.uptime"]);
+   return execRes.stdout;
+}
+
+async function getPidAndWS(jreHome: string, context:vscode.ExtensionContext) {
+   const jpsExecRes = await execFile(path.join(jreHome, "bin", "jps"), ["-v"]);
+   const jdtlsLines = jpsExecRes.stdout.split(os.EOL).filter(line => line.includes("org.eclipse.jdt.ls.core"));
+   let jdtlsJpsLine;
+   if (context.storageUri) {
+      jdtlsJpsLine = jdtlsLines.find(line => line.includes(path.dirname(context.storageUri!.fsPath)));
+   } else {
+      jdtlsJpsLine = jdtlsLines.find(line => line.includes("vscodesws_"));
+   }
+
+   return jdtlsJpsLine ? parseJdtlsJps(jdtlsJpsLine) : {};
 }
--- src/daemon/logWatcher.ts
+++ src/daemon/logWatcher.ts
@@ -1,10 +1,12 @@
 import * as path from "path";
 import * as fs from "fs";
+import * as os from "os";
 import * as vscode from "vscode";
 import { sendInfo } from "vscode-extension-telemetry-wrapper";
 
 export class LogWatcher {
     private serverLogUri: vscode.Uri | undefined;
+    private offset: number = 0;
 
     constructor(context: vscode.ExtensionContext) {
         if (context.storageUri) {
@@ -25,10 +27,20 @@ export class LogWatcher {
 
         try {
             await fs.promises.access(this.serverLogUri.fsPath);
-            const watcher = fs.watch(this.serverLogUri.fsPath);
-            watcher.on("change", (eventType, filename) => {
-                console.log(eventType, filename);
-            })
+            const logPath = this.serverLogUri.fsPath;
+            setInterval(async () => {
+                const content = await fs.promises.readFile(logPath, {encoding: 'utf-8'});
+                if (!content.endsWith(os.EOL)) {return;}
+                const newContent = content.slice(this.offset);
+                this.offset = content.length; 
+
+                const m = newContent.match(/.*exception.*/i);
+                if (m) {
+                    const start = m.index;
+                    const end = newContent.slice(start).indexOf(`${os.EOL}${os.EOL}`);
+                    console.log(newContent.slice(start, end));
+                }
+            }, 5000)
         } catch (error) {
             sendInfo("", {name: "no-server-log"});
         }
