3bf5da3d476f9ad0944c62292c7f95fc1fa7f9c0
wangmi <wangmi@microsoft.com>
2024-04-19 21:55:39 +0800

    add code action to inspect java code selection

--- src/copilot/inspect/commands.ts
+++ src/copilot/inspect/commands.ts
@@ -1,12 +1,18 @@
-import { DocumentSymbol, TextDocument } from "vscode";
+import { DocumentSymbol, TextDocument, Range, Selection } from "vscode";
 import { instrumentOperationAsVsCodeCommand } from "vscode-extension-telemetry-wrapper";
 import InspectionCopilot from "./InspectionCopilot";
 
 export const COMMAND_INSPECT_CLASS = 'java.copilot.inspect.class';
+export const COMMAND_INSPECT_RANGE = 'java.copilot.inspect.range';
 
 export function registerCommands() {
     instrumentOperationAsVsCodeCommand(COMMAND_INSPECT_CLASS, async (document: TextDocument, clazz: DocumentSymbol) => {
         const copilot = new InspectionCopilot();
         void copilot.inspectClass(document, clazz);
+    });
+
+    instrumentOperationAsVsCodeCommand(COMMAND_INSPECT_RANGE, async (document: TextDocument, range: Range | Selection) => {
+        const copilot = new InspectionCopilot();
+        void copilot.inspectRange(document, range);
     });
 }
--- src/copilot/inspect/index.ts
+++ src/copilot/inspect/index.ts
@@ -1,5 +1,5 @@
-import { ExtensionContext, window, workspace } from "vscode";
-import { registerCommands } from "./commands";
+import { CancellationToken, CodeAction, CodeActionContext, CodeActionKind, ExtensionContext, TextDocument, languages, window, workspace, Range, Selection } from "vscode";
+import { COMMAND_INSPECT_RANGE, registerCommands } from "./commands";
 import { InspectActionCodeLensProvider } from "./InspectActionCodeLensProvider";
 
 export const DEPENDENT_EXTENSIONS = ['github.copilot-chat', 'redhat.java'];
@@ -12,6 +12,20 @@ export async function activateCopilotInspection(context: ExtensionContext): Prom
 
     context.subscriptions.push(
         workspace.onDidOpenTextDocument(doc => inspectActionCodeLenses.rerender(doc)), // Rerender class codelens
+        languages.registerCodeActionsProvider({ language: 'java' }, { provideCodeActions: inspectUsingCopilot }), // Inspect using Copilot
     );
     window.visibleTextEditors.forEach(editor => inspectActionCodeLenses.rerender(editor.document));
+}
+
+async function inspectUsingCopilot(document: TextDocument, range: Range | Selection, _context: CodeActionContext, _token: CancellationToken): Promise<CodeAction[]> {
+    const action: CodeAction = {
+        title: "Rewrite with new syntax",
+        kind: CodeActionKind.RefactorRewrite,
+        command: {
+            title: "Rewrite selected code using Copilot",
+            command: COMMAND_INSPECT_RANGE,
+            arguments: [document, range]
+        }
+    };
+    return [action];
 }
